5f6f71d6663ee23c435aa93cf7fa25feec60f5ba
Amele06 <kelvina0306@gmail.com>
2023-07-21 08:49:31 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isupper - function that checks for uppercase character
+ * _isupper - function that checks for uppercase character
  * @c: parameter to be checked
  * Return: 1 or 0
  */
