fbf1582a3b380ef270c0ae809f2c16e359545dde
Amele06 <kelvina0306@gmail.com>
2023-07-27 08:38:39 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len = strlen(dest);
 	int i;
+	int j;
 
-	for (i = 0 ; i < n && *src != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest[len + i] = src[i];
-		src++;
+		i++;
 	}
-	dest[len + i] = '\0';
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
