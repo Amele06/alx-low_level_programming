d4256f3a51882a590371f9e19d3f5e197515a73d
Amele06 <kelvina0306@gmail.com>
2023-08-10 13:24:42 -0700

    allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  function that allocates memory using malloc
+ * @b: memory to be allocated
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void * ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
