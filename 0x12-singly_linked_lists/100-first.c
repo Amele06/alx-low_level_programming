00d61186875af747ba0ecdc2e6236d135f3b5174
Amele06 <kelvina0306@gmail.com>
2023-08-25 11:37:16 -0700

    function that prints a sentence

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
+void first(void)__attribute__((constructor));
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - prints a sentence before the main function is executed
  */
+
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
