02f2f31c0e428c163745d9fe38d5d6af953920db
Amele06 <kelvina0306@gmail.com>
2024-05-11 06:18:22 -0700

    function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,25 +1,37 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in a sorted array of integers
- * @array: array of integers
- * @size: size of array
- * @value: value to search for
- * Return: index of value or -1 if not found
+ * linear_search - Searches for a value in an array
+ *                 of integers using linear search.
+ * @array: A pointer to the first element of the array to search.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the array is NULL, -1.
+ *         Otherwise, the first index where the value is located.
  */
-
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t inc;
+	size_t i;
 
+	/* Check if the array is NULL */
 	if (array == NULL)
 		return (-1);
-	for (inc = 0; inc < size; inc++)
+
+	/* Iterate over each element of the array */
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
-		if (array[inc] == value)
-			return (inc);
+		/* Print the value being checked */
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		/* If the value is found */
+		if (array[i] == value)
+
+			/* Return the index of the value */
+			return (i);
 	}
+
+	/* Value not found, return -1 */
 	return (-1);
 }
